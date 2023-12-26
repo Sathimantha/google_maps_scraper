ba08c8b70fa6a11ae9cdb26d12c03921cbd95cdf
Sathimantha Malalaskera <sathimantha@gmail.com>
2023-12-27 00:41:10 +0530

    Directory support and maps url

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from playwright.sync_api import sync_playwright
 from dataclasses import dataclass, asdict, field
 import pandas as pd
 import argparse
+from pathlib import Path
 
 
 @dataclass
@@ -15,9 +16,9 @@ class Business:
     address: str = None
     website: str = None
     phone_number: str = None
-    reviews_count: int = None
-    reviews_average: float = None
-
+#    reviews_count: int = None
+#    reviews_average: float = None
+    maps_url: str = None  # New attribute for Google Maps URL
 
 @dataclass
 class BusinessList:
@@ -36,31 +37,31 @@ class BusinessList:
             (asdict(business) for business in self.business_list), sep="_"
         )
 
-    def save_to_excel(self, filename):
+    def save_to_excel(self, filename , directory):
         """saves pandas dataframe to excel (xlsx) file
 
         Args:
             filename (str): filename
         """
-        self.dataframe().to_excel(f"{filename}.xlsx", index=False)
+        self.dataframe().to_excel(f"{directory}{filename}.xlsx", index=False)
 
-    def save_to_csv(self, filename):
+    #def save_to_csv(self, filename):
         """saves pandas dataframe to csv file
 
         Args:
             filename (str): filename
         """
-        self.dataframe().to_csv(f"{filename}.csv", index=False)
+    #    self.dataframe().to_csv(f"{filename}.csv", index=False)
 
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
         page.goto("https://www.google.com/maps", timeout=60000)
         # wait is added for dev phase. can remove it in production
-        page.wait_for_timeout(5000)
+        #page.wait_for_timeout(5000)
 
         page.locator('//input[@id="searchboxinput"]').fill(search_for)
         page.wait_for_timeout(3000)
@@ -126,7 +127,9 @@ def main():
             address_xpath = '//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
             website_xpath = '//a[@data-item-id="authority"]//div[contains(@class, "fontBodyMedium")]'
             phone_number_xpath = '//button[contains(@data-item-id, "phone:tel:")]//div[contains(@class, "fontBodyMedium")]'
-            reviews_span_xpath = '//span[@role="img"]'
+        #    reviews_span_xpath = '//span[@role="img"]'
+            maps_url_xpath = '//a[contains(@href, "https://www.google.com/maps/place")]'
+
 
             business = Business()
 
@@ -146,31 +149,36 @@ def main():
                 business.phone_number = page.locator(phone_number_xpath).inner_text()
             else:
                 business.phone_number = ""
-            if listing.locator(reviews_span_xpath).count() > 0:
-                business.reviews_average = float(
-                    listing.locator(reviews_span_xpath)
-                    .get_attribute("aria-label")
-                    .split()[0]
-                    .replace(",", ".")
-                    .strip()
-                )
-                try:
-                   business.reviews_count = int(
-                    listing.locator(reviews_span_xpath)
-                    .get_attribute("aria-label")
-                    .split()[2]
-                    .strip()
-                )
-                except:
-                    business.reviews_count=0
+            #if listing.locator(reviews_span_xpath).count() > 0:
+            #    business.reviews_average = float(
+            #        listing.locator(reviews_span_xpath)
+            #        .get_attribute("aria-label")
+            #        .split()[0]
+            #        .replace(",", ".")
+            #        .strip()
+            #    )
+            #    try:
+            #       business.reviews_count = int(
+            #        listing.locator(reviews_span_xpath)
+            #        .get_attribute("aria-label")
+            #        .split()[2]
+            #        .strip()
+            #    )
+            #    except:
+            #        business.reviews_count=0
+            #else:
+            #    business.reviews_average = ""
+            #    business.reviews_count = ""
+            
+            if listing.locator(maps_url_xpath).count() > 0:
+                business.maps_url = listing.locator(maps_url_xpath).get_attribute("href")
             else:
-                business.reviews_average = ""
-                business.reviews_count = ""
+                business.maps_url = ""
 
             business_list.business_list.append(business)
 
         # saving to both excel and csv just to showcase the features.
-        business_list.save_to_excel(search_for)
+        business_list.save_to_excel(search_for,directory)
         #business_list.save_to_csv("google_maps_data")
 
         browser.close()
@@ -180,6 +188,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--search", type=str)
     parser.add_argument("-t", "--total", type=int)
+    parser.add_argument("-d", "--directory", type=str)
     args = parser.parse_args()
 
     if args.search:
@@ -194,5 +203,15 @@ if __name__ == "__main__":
         total = args.total
     else:
         total = 10
+    
+
+
+
+    if args.directory:
+        directory = "out/"+args.directory+"/"
+    else:
+        directory = "out/"
+    
+    Path(directory).mkdir(parents=True, exist_ok=True)
 
     main()
